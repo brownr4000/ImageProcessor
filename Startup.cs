1d1961d3f125139d9ad87671035b5f267d337507
Robert Brown <rbrown3@seattleu.edu>
2022-05-19 15:35:29 -0700

    Adding comments to Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,9 @@
+/*
+ * Robert (Bob) Brown rbrown3
+ * CPSC 5200-01-SQ22, Seattle University
+ * This is free and unencumbered software release into the public domain.
+ */
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +17,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+// AUTHOR:      Bob Brown rbrown3
+// PROGRAM:     ImageProcessor API
+// DATE:        2022-05-18
+// PURPOSE:     
+// INPUT:       
+// PROCESS:     
+// OUTPUT:      
+//
 
 namespace ImageProcessor
 {
