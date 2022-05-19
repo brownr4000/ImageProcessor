2ae773736c52cc1bfa215d998abcbcf46a6c9412
Robert Brown <rbrown3@seattleu.edu>
2022-05-19 16:11:25 -0700

    Created RequestController.cs

---
+++ Controllers/RequestController.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Robert (Bob) Brown rbrown3
+ * CPSC 5200-01-SQ22, Seattle University
+ * This is free and unencumbered software release into the public domain.
+ */
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// AUTHOR:      Bob Brown rbrown3
+// PROGRAM:     ImageProcessor API
+// DATE:        2022-05-18
+// PURPOSE:     
+// INPUT:       
+// PROCESS:     
+// OUTPUT:      
+//
+
+namespace ImageProcessor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
