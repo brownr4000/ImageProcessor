ba46944492df678cea6d5e90ec442e1f0c550e41
Robert Brown <rbrown3@seattleu.edu>
2022-05-19 16:44:25 -0700

    Added using to OperatorModel.cs

--- Models/OperatorModel.cs
+++ Models/OperatorModel.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SixLabors.ImageSharp;
 
 // AUTHOR:      Bob Brown rbrown3
 // PROGRAM:     ImageProcessor API
