498599d2a47e34878e36c2f4f81a9195b107a1ce
Robert Brown <rbrown3@seattleu.edu>
2022-05-19 16:24:47 -0700

    Add OperatorModel.cs to the project

---
+++ Models/OperatorModel.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Robert (Bob) Brown rbrown3
+ * CPSC 5200-01-SQ22, Seattle University
+ * This is free and unencumbered software release into the public domain.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// AUTHOR:      Bob Brown rbrown3
+// PROGRAM:     ImageProcessor API
+// DATE:        2022-05-18
+// PURPOSE:     
+// INPUT:       
+// PROCESS:     
+// OUTPUT:      
+//
+
+namespace ImageProcessor.Models
+{
+    public class OperatorModel
+    {
+    }
+}
