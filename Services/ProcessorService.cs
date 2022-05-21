618bed627859787b647b5d7ce535261778cabd1a
Robert Brown <rbrown3@seattleu.edu>
2022-05-20 23:29:22 -0700

    Created ProcessorService.cs

---
+++ Services/ProcessorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+
+using SixLabors.ImageSharp.Processing;
+using ImageProcessor.Models;
+
+namespace ImageProcessor.Services
+{
+    public class ProcessorService
+    {
+        public ProcessorService()
+        {
+
+        }
+
+        private OperatorModel picture;
+
+        private Image image;
+
+        public Image performFlip(string dir)
+        {
+            FlipMode direction = FlipMode.Vertical;
+
+            if (dir.ToLower().StartsWith("h"))
+                direction = FlipMode.Horizontal;
+
+            image.Mutate(x => x
+                    .Flip(direction));
+
+            image.Save("output/fb.png"); // Automatic encoder selected based on extension.
+
+            return image;
+        }
+
+    }
+}
