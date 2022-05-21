c3c51a3353ba296d6c27307c66ca82235e063db9
Robert Brown <rbrown3@seattleu.edu>
2022-05-20 18:54:00 -0700

    Updating constructor and fields of OperatorModel.cs

--- Models/OperatorModel.cs
+++ Models/OperatorModel.cs
@@ -13,25 +13,36 @@ using SixLabors.ImageSharp;
 // AUTHOR:      Bob Brown rbrown3
 // PROGRAM:     ImageProcessor API
 // DATE:        2022-05-18
-// PURPOSE:     
-// INPUT:       
-// PROCESS:     
-// OUTPUT:      
 //
 
 namespace ImageProcessor.Models
 {
+    /// <summary>
+    /// The OperatorModel class defines an OperatorModel object that stores and Image
+    /// and a list of commands
+    /// </summary>
     public class OperatorModel
     {
+        /// <summary>
+        /// The OperatorModel constructor stores an image and a list of strings
+        /// </summary>
+        /// <param name="picture">An Image to perform operations on</param>
+        /// <param name="process">A list of strings of operations to perform</param>
+        public OperatorModel(Image picture, List<string> commands)
+        {
+            command = commands;
+
+            image = picture;
+        }
+
+        /// <summary>
+        /// The list of strings to hold the image manipulation commands
+        /// </summary>
+        public List<string> command { get; set; }
+
+        /// <summary>
+        /// The Image to be manipulated
+        /// </summary>
+        private Image image { get; set; }
     }
-
-    // This should hold the commmand list and image!
-    // This is the operator object!
-
-    /// <summary>
-    /// The list of strings to hold the image manipulation commands
-    /// </summary>
-    public List<string> command { get; set; }
-
-    private Image image;
 }
